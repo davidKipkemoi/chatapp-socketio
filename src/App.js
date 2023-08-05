8a6592a255e8cac3efc04f43358a6309ea7bcdfd
Kiprich <davidkipkemoi30@gmail.com>
2023-08-05 19:30:06 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from "antd"
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button type="primary">Primary Button</Button>
       </header>
     </div>
   );
---
+++ src/components/ChatBox.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Avatar, Image} from 'antd'
+
+export default function ChatBoxReceiver() {
+  return (
+    <div style={{display:'flex',justifyContent:'flex-start',flexDirection:'row'}} >
+        <Avatar>
+            siz
+        </Avatar>
+        
+        </div>
+  )
+}
+
+export default function ChatBoxSender() {
+    return (
+      <div>ChatBox</div>
+    )
+  }
+  
