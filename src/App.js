d40559c065b8048f0acf8df83c319e237ffd569a
Kiprich <davidkipkemoi30@gmail.com>
2023-08-05 21:43:58 +0300

    Chatbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from "antd"
+import ChatBoxReceiver,{ChatBoxSender} from "./components/ChatBox";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button type="primary">Primary Button</Button>
-      </header>
-    </div>
+  
+   
+   
+    <div style={{background:"black", height:"100vH"}}>
+    <ChatBoxReceiver user="David" 
+    avatar="https://img.freepik.com/free-photo/fashion-boy-with-yellow-jacket-blue-pants_71767-96.jpg?w=740&t=st=1691254842~exp=1691255442~hmac=25c847fca8b3d60c1c5ae65b8572d9c8321ea61037dcb273204b03eed45f410f" 
+    message="Tubonge"/>
+
+    <ChatBoxSender user="Kiprich" 
+     avatar="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" 
+     message="Niambie"/>
+   </div>
+ 
   );
 }
 
--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,5 +1,28 @@
 import React from 'react'
-
+const styles={
+  button: {
+    width:'10%' ,
+    height:50 ,
+    fontWeight:'bold', 
+    borderRadius:10 ,
+    fontSize:18 ,
+    backgroundColor:'#34b7f1',
+    borderWidth:0,
+    color:'#fff'
+  },
+  textarea:{ 
+     width:'60%' ,
+     height:50 ,
+     borderRadius:10, 
+     borderWidth:0 , 
+     padding:10 , 
+     fontSize:18
+    },
+  textContainer:{
+    display:"flex", 
+    justifyContent:'space-evenly', 
+    alignItems:'center'}
+}
 export default function InputText() {
   return (
     <div>InputText</div>
