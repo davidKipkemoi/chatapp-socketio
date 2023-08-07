d9872f04a58b4be1a4c325ef8cd828745d4b2089
Kiprich <davidkipkemoi30@gmail.com>
2023-08-07 03:49:02 +0300

    firebase connection

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,38 +1,92 @@
-import React, { useEffect, useState } from 'react'
-import socketIOClient from "socket.io-client"
-import ChatBoxReceiver,{ChatBoxSender} from  './ChatBox';
-import UserLogin from "./UserLogin"
-import InputText from "./InputText";
+import React, { useEffect, useState, useRef } from 'react'
+import socketIOClient from "socket.io-client";
+import ChatBoxReceiver, { ChatBoxSender } from './ChatBox';
+import InputText from './InputText';
+import UserLogin from './UserLogin';
+import {
+    doc,
+    setDoc,
+    collection,
+    serverTimestamp,
+    query,
+    onSnapshot,
+    orderBy,
+  
+  } from 'firebase/firestore';
+  import db from "./firebaseConfig/firebaseConfig.js"
 
 
 export default function ChatContainer() {
-  let socketio = socketIOClient("http://localhost:5000")
-  const[chats,setChats]=useState([])
-  const[user,setUser]=useState(localStorage.getItem("user"))
-  const avatar=(localStorage.getItem("avatar"))
+  
+    let socketio  = socketIOClient("http://localhost:6001")
+    const [chats , setChats] = useState([])
+    const [user, setUser] = useState(localStorage.getItem("user"))
+    const avatar = localStorage.getItem('avatar')
+    const chatsRef = collection(db , "Messages")
+    const messagesEndRef = useRef(null)
+    const scrollToBottom = () => {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }    
 
-  useEffect(()=>{
-    socketio.on("chat",senderChats=>{
-      setChats(senderChats)
+    useEffect(() => {
+      scrollToBottom()
+    }, [chats])
+
+    useEffect(()=> {
+        socketio.on('chat', senderChats => {
+            setChats(senderChats)
+        })
     })
 
+
+    useEffect(()=>{
+
+        const q = query(chatsRef , orderBy('createdAt' , 'asc'))
+      
+        const unsub = onSnapshot(q, (querySnapshot) =>{
+          const fireChats =[]
+          querySnapshot.forEach(doc => {
+            fireChats.push(doc.data())
+          });
+         setChats([...fireChats])
+        })
+        return ()=> {
+          unsub()
+        }
+      
+      },[])
+
+     function addToFirebase(chat){
+        const newChat = {
+            avatar,
+            createdAt: serverTimestamp(),
+            user,
+            message: chat.message
+        }
+
+        const chatRef = doc(chatsRef)
+        setDoc(chatRef , newChat)
+        .then(()=> console.log('Chat added succesfully'))
+        .catch(console.log)
+     } 
    
-  })
-  function sendChatToSocket(chat){
-    socketio.emit('chat',chat)
-  }
 
-  function addMessage(chat){
-    const newChat = {...chat, user:localStorage.getItem("user"),avatar}
-    setChats([...chats,newChat])
-    sendChatToSocket([...chats,newChat])
-  }
+    function sendChatToSocket(chat){
+        socketio.emit("chat" , chat)
+    }
 
-  function handleLogout(){
-    localStorage.removeItem("user")
-    localStorage.removeItem("avatar")
-    setUser("")
-  }
+    function addMessage(chat){
+        const newChat = {...chat , user:localStorage.getItem("user") , avatar}
+        addToFirebase(chat)
+        setChats([...chats , newChat])
+        sendChatToSocket([...chats , newChat])
+    }
+
+    function handleLogout(){
+        localStorage.removeItem("user")
+        localStorage.removeItem("avatar")
+        setUser("")   
+    }
   function ChatsList(){
     return  (<div>
     {
